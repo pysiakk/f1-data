07e7272102d31f2882d7ef4225d4f34c3991306b
pysiakk <karol.pysiak.13@gmail.com>
2023-11-11 18:31:13 +0100

    pretty update_index

--- update_index.py
+++ update_index.py
@@ -1,7 +1,4 @@
-# %%
 import os
-
-from pathlib import Path
 import glob
 
 
@@ -10,7 +7,7 @@ def generate_tree(path, html=""):
     for dir in os.listdir("figs"):
         html += f"<h3>figs/{dir}</h3>"
         html += "<ul>"
-        files = glob.glob(os.path.join("figs", dir) + '/*')
+        files = glob.glob(os.path.join("figs", dir) + "/*")
         files = sorted(files, key=os.path.getctime)
         for file in files:
             html += f'<li><a href="{file}">{os.path.basename(file)}</a></li>'
@@ -19,7 +16,5 @@ def generate_tree(path, html=""):
     return html
 
 
-# %%
 with open("index.html", "w") as f:
     f.write(generate_tree("."))
-# %%
